aee1e32c7b62ee9b26ea7f265f29b48a9f0c05d4
Lauri Oksanen <lauri.oksanen@helsinki.fi>
2022-03-06 20:06:58 +0200

    Workaround for terminfo

--- install-fish.sh
+++ install-fish.sh
@@ -9,5 +9,13 @@ mkdir -p $bindir/fish-portable
 pushd $bindir
 curl -LO https://github.com/xxh/fish-portable/releases/download/3.3.1/fish-portable-musl-alpine-Linux-x86_64.tar.gz
 tar xf fish-portable-musl-alpine-Linux-x86_64.tar.gz --directory=fish-portable
-ln -s fish-portable/bin/fish fish
+
+cat > $bindir/fish <<'EOF'
+#!/bin/sh
+export TERMINFO_DIRS=/lib/terminfo:/etc/terminfo:/usr/share/terminfo:$TERMINFO_DIRS
+CURRENT_DIR="$(cd "$(dirname "$0")" && pwd)"
+$CURRENT_DIR/fish-portable/bin/fish "$@"
+EOF
+
+chmod +x $bindir/fish
 popd

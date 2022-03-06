ca787bfa6cf12052a4502c4927a5dee408782680
Lauri Oksanen <lauri.oksanen@helsinki.fi>
2022-03-06 22:14:57 +0200

    Fixed misprints

--- install.fish
+++ install.fish
@@ -1,4 +1,4 @@
-fzf_version=0.29.0
+set fzf_version 0.29.0
 
 set srcdir (realpath (status dirname))
 source $srcdir/fish/functions/symlink.fish
@@ -6,5 +6,5 @@ symlink $HOME/.config/fish/config.fish $srcdir/fish/config.fish
 
 set bindir (realpath (dirname (status fish-path))/../../)
 pushd $bindir
-curl -L https://github.com/junegunn/fzf/releases/download/$fzf_version/fzf-$fzf_version-linux_amd64.tar.gz | tar x 
+curl -L https://github.com/junegunn/fzf/releases/download/$fzf_version/fzf-$fzf_version-linux_amd64.tar.gz | tar xz 
 popd
